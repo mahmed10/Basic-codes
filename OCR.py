d0c97a6010f83ed81da8c1bfde8e7ab23a8a47f0
Masud Ahmed <53001400+medy7@users.noreply.github.com>
2019-09-19 00:11:21 +0900

    Create OCR.py

---
+++ OCR.py
@@ -0,0 +1,8 @@
+'''
+setup link: https://github.com/UB-Mannheim/tesseract/wiki
+https://github.com/tesseract-ocr/tesseract/wiki
+youtube: https://www.youtube.com/watch?v=Rb93uLXiTwA
+somecodes: https://pypi.org/project/pytesseract/
+'''
+import pytesseract
+print(pytesseract.image_to_string(Image))# Simple image to string
