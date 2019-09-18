3934ad79eef354f571cd5d5628043214c8de658c
Masud Ahmed <53001400+medy7@users.noreply.github.com>
2019-09-18 22:30:27 +0900

    Create FolderCreate.py

---
+++ FolderCreate.py
@@ -0,0 +1,7 @@
+import os
+try:  
+    os.mkdir(output_path)
+except OSError:  
+    logging.info(os.path.abspath(output_path) + ' folder already existed')
+else:
+	logging.info(os.path.abspath(output_path) + ' folder created')
